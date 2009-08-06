4f3dece67166e90cc9958a7aeca73f4d529402de
Matias P. Brutti <matiasbrutti@gmail.com>
2009-08-06 08:48:42 -0300

    working ... thinking ...

--- lib/task.rb
+++ lib/task.rb
@@ -1,18 +1,20 @@
 module Sicherheit
   class Task
-    attr_accessor :command, :type, :on, :args, :targets
+    attr_accessor :command, :type, :on, :args, :targets, :runon, :dependencies
     
-    def initialize(command, args, type = :async, on = :localhost, &blocks)
-      if (option.is_a?(Symbol) || option.is_a?(String))
-        block_given? ? block.call : raise ArgumentError, "Need a Block"  
-      else
+    def initialize(option={}, &block)
+      if option.is_a?(Symbol) || option.is_a?(String)
+        self.command = option
+        block_given? ? block.call(self) : raise(ArgumentError, "Need a Block")  
+        self.run
+      elsif option.is_a?(Hash)
         self.command = option[:command]
         self.type = option[:type]
         self.on = option[:on]
         self.args = option[args]
         self.targets = option[:targets]
-        self.runon = hsh[:runon]
-        self.dependencies = hsh[:dependencies]
+        self.runon = option[:runon]
+        self.dependencies = option[:dependencies]
       else
         raise ArgumentError, "Need a Hash or a Block"
       end 
@@ -20,15 +22,30 @@ module Sicherheit
     
     def run
       #TODO: run command on server on $server
+      puts "This is a test"
     end
   
     def results
       #Return results.
     end
-    
   end
 end
 
 
 
-Sicherheit::Tasks.new "nmap"  
+Sicherheit::Task.new "nmap" do |t|
+  t.args = "-sS -P0 -p1-1024"
+  t.type = :async 
+  t.on = :localhost
+  t.targets = ["192.168.1.1", "192.168.1.2"]
+  t.runon = :Sateada
+end
+
+a = Sicherheit::Task.new :command => "nmap",
+                         :args => "-sS -P0 -p1-1024",
+                         :type => :async,
+                         :on => :localhost,
+                         :targets => ["192.168.1.1", "192.168.1.2"],
+                         :runon => :Sateada
+
+a.run
